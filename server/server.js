e17c0b37cf39395220c7934f126069b7495edf61
iboruch <99720418+iboruch@users.noreply.github.com>
2026-02-17 10:08:05 +0100

    update PR

--- server/server.js
+++ server/server.js
@@ -43,10 +43,8 @@ app.use(cors({
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('Connected to MongoDB'))
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error(err));
 
 // In-memory session store (to replace sessions logic)
